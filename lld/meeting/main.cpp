26f7865a5234e4c7cca47c4167bfc506d931c768
satyam.kumar <satyam.kumar@siply.in>
2023-02-24 04:29:09 +0530

    poor approach

--- lld/meeting/main.cpp
+++ lld/meeting/main.cpp
@@ -6,7 +6,7 @@ typedef vector<Meeting> Calendar;
 class Room {
 public:
     Room(string);
-    bool book(int, int);
+    bool book(int, int, int);
     Calendar getCalendar() const;
     void setCalendar(Calendar calendar);
     string getName() const;
@@ -19,16 +19,18 @@ private:
 
 class Meeting {
 public:
-    Meeting(int, int, Room);
+    Meeting(int, int, int, Room);
     int getEnd() const;
     void setEnd(int end);
     const Room& getRoom() const;
     void setRoom(const Room& room);
     int getStart() const;
     void setStart(int start);
+    int getDay() const;
+    void setDay(int day);
 
 private:
-    int start, end;
+    int start, end, day;
     Room room;
 };
 
@@ -36,8 +38,9 @@ class Scheduler {
     vector<Room>rooms;
 public:
     Scheduler(vector<Room>);
-    string book(int, int);
+    string book(int, int, int);
 };
+
 int main() {
     Room room1("adarsh");
     Room room2("utkramit");
@@ -46,12 +49,12 @@ int main() {
 
     Scheduler scheduler(roomVec);
 
-    cout<<scheduler.book(2, 5)<<"\n";
-    cout<<scheduler.book(5, 8)<<"\n";
-    cout<<scheduler.book(4, 8)<<"\n";
-    cout<<scheduler.book(3, 6)<<"\n";
-    cout<<scheduler.book(7, 8)<<"\n";
-    cout<<scheduler.book(6, 9)<<"\n";
+    cout<<scheduler.book(2, 5, 1)<<"\n";
+    cout<<scheduler.book(5, 8, 2)<<"\n";
+    cout<<scheduler.book(4, 8, 1)<<"\n";
+    cout<<scheduler.book(3, 6, 2)<<"\n";
+    cout<<scheduler.book(7, 8, 1)<<"\n";
+    cout<<scheduler.book(6, 9, 2)<<"\n";
     return 0;
 }
 
@@ -75,25 +78,26 @@ void Room::setName(string name) {
     this->name = name;
 }
 
-bool Room::book(int start, int  end) {
+bool Room::book(int start, int  end, int day) {
     Calendar calendar = getCalendar();
     for (Meeting m : calendar) {
-        if (m.getStart()<end&&start<m.getEnd()) {
+        if (m.getDay()==day&&m.getStart()<end&&start<m.getEnd()) {
             return false;
         }
     }
 
-    Meeting meeting(start, end, *this);
+    Meeting meeting(start, end, day, *this);
     calendar.push_back(meeting);
     setCalendar(calendar);
     return true;
 }
 
-Meeting::Meeting(int start, int end, Room room):room(room) {
+Meeting::Meeting(int start, int end, int day, Room room):room(room) { //passed copy here
     // this->room = room; //? this wont work because
     //constructor for 'Meeting' must explicitly initialize the member 'room' which does not have a default constructor
     this->start = start;
     this->end = end;
+    this->day = day;
 }
 
 int Meeting::getEnd() const {
@@ -120,13 +124,22 @@ void Meeting::setStart(int start) {
     this->start = start;
 }
 
+int Meeting::getDay() const {
+    return day;
+}
+
+void Meeting::setDay(int day) {
+    this->day = day;
+}
+
+
 Scheduler::Scheduler(vector<Room> rooms) {
     this->rooms = rooms;
 }
 
-string Scheduler::book(int start, int end) {
+string Scheduler::book(int start, int end, int day) {
     for (Room& room: rooms) {
-        bool flag = room.book(start, end);
+        bool flag = room.book(start, end, day);
         if (flag==true) {
             return room.getName();
         }
